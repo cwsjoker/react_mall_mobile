4d6f466c4934d5a9398afbf4bf7626cf7c4157af
caiweishao <419436210@qq.com>
2019-05-06 10:05:14 +0800

    登录样式

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { Link }  from 'react-router-dom';
+// import { List, InputItem, WhiteSpace } from 'antd-mobile';
+// import { createForm } from 'rc-form';
+
+import './login.scss';
+import logo from '../../assets/images/logo.png';
 
 class Login extends Component {
     render() {
+        // const { getFieldProps } = this.props.form;
         return (
-            <div className="loginPage">登录页面</div>
+            <div className="loginPage">
+                <div className="login-logo">
+                    <div className="logo">
+                        {/* <img src={logo} /> */}
+                    </div>
+                    <h2>登录</h2>
+                </div>
+                <div className="login-form">
+                    <div className="input-item">
+                        <input type="number" placeholder="请输入手机号码" />
+                    </div>
+                    <div className="input-item">
+                        <input type="password" placeholder="密码" />
+                    </div>
+                </div>
+                <div className="login-submit">登录</div>
+                <div className="login-footer">
+                    您还不是bttmall用户?
+                    <Link to={'/register'}>立即注册</Link>
+                </div>
+            </div>
         )
     }
 }
 
+// export default createForm()(Login);
 export default Login;
