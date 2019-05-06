ec47a932483eb9aeef1bbaf65e9cefb8a4e97d32
caiweishao <419436210@qq.com>
2019-05-06 10:23:17 +0800

    注册样式

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+import './register.scss';
+
 class Register extends Component {
     render() {
         return (
-            <div className="registerPage">注册页面</div>
+            <div className="registerPage">
+                <div className="register-logo">
+                    <div className="logo">
+                        {/* <img src={logo} /> */}
+                    </div>
+                    <h2>注册</h2>
+                </div>
+                <div className="register-form">
+                    <div className="input-item">
+                        <input type="number" placeholder="请输入手机号码" />
+                    </div>
+                    <div className="input-item">
+                        <input type="number" placeholder="请输入登录密码" />
+                    </div>
+                    <div className="input-tip">至少6位字符，非纯数字</div>
+                    <div className="input-item">
+                        <input type="number" placeholder="请输入交易密码" />
+                    </div>
+                    <div className="input-tip">用于提现，请牢记。不可与登录密码相同</div>
+                </div>
+            </div>
         )
     }
 }
